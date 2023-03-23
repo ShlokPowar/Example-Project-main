63567c0fe110c37be25ee2e16456fb578440a42e
Shlok <shlokpow@gmail.com>
2023-03-23 15:28:08 -0500

    d

--- src/main.cpp
+++ src/main.cpp
@@ -77,10 +77,10 @@ void initialize() {
 
   // Autonomous Selector using LLEMU
   ez::as::auton_selector.add_autons({
-    Auton("Roller side auton (win point)\n\n", solo_wp),
+    Auton("Roller side auton (win point)\n\n", other_skills),
     Auton("Roller side auton (normal)", roller_side_single),
     Auton("Not roller side auton\n\n",not_roller_side),
-    Auton("Skills\n\n", other_skills),
+    Auton("Skills\n\n", solo_wp),
     Auton("Skills (test)\n\n", roller_side_single)
   });
 
